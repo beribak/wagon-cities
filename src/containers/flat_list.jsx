ee73dc942b171546e7df82d76ecd3dd9bdc384ad
Danko Beribak <beribakdanko@gmail.com>
2019-10-31 05:29:05 +0100

    react redux setup

---
+++ src/containers/flat_list.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class FlatList extends Component {
+
+  render() {
+  	return(
+  		<div className="col-sm-7">
+  		  TODO
+  		</div>
+	);
+  }
+}
+
+export default FlatList;
