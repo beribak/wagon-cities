1c2026ba1ecc90a4f870cca5b5c2c9d8d37ea665
Danko Beribak <beribakdanko@gmail.com>
2019-11-02 00:09:51 -0700

    map marker finished

--- src/containers/map.jsx
+++ src/containers/map.jsx
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import { connect } from 'react-redux';
 
 class Map extends Component {
   render() {
-    const marker = null;
-    const center = { lat: 48.856614, lng: 2.352222 };
+    let marker = null;
+    let center = {lat: 48.856, lng: 2.352};
+    
+    if (this.props.selectedFlat) {
+      marker = (
+        <div
+          style={{
+            width: '20px',
+            height: '20px',
+            backgroundColor: 'red',
+            borderRadius: '50%'}}
+          lat={this.props.selectedFlat.lat}
+          lng={this.props.selectedFlat.lng}
+        />
+      );
+      center = {
+        lat: this.props.selectedFlat.lat,
+        lng: this.props.selectedFlat.lng
+      };
+    }
+
     return (
-      <div className="col-sm-5" style={{ height: '100vh' }}>
-        <GoogleMapReact
-          center={center}
-          defaultZoom={15}
-        >
-          {marker}
-        </GoogleMapReact>
-      </div>
+        <div className="col-sm-5" style={{ height: '100vh', position: 'fixed', top: '0', right: '0', padding: '0px' }}>
+          <GoogleMapReact
+            center={center}
+            defaultZoom={15}
+          >
+            {marker}
+          </GoogleMapReact>
+        </div>  
     );
   }
 }
 
-export default Map;
+function mapStateToProps(state) {
+  return {
+    selectedFlat: state.selectedFlat
+  };
+}
+
+export default connect(mapStateToProps)(Map);
